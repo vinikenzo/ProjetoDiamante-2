db94765b264954fd5f60220125a57feabd0249ad
João Victor <joaovictoralves087@gmail.com>
2026-04-21 09:26:12 -0300

    Habilitando eureka server

--- eurekaserver/eurekaserver/src/main/java/br/com/restaurante/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/eurekaserver/src/main/java/br/com/restaurante/eurekaserver/EurekaserverApplication.java
@@ -2,8 +2,10 @@ package br.com.restaurante.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
